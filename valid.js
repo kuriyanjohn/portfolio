48b84d3a31c4adba4e410a7ecc6f3e7df9beb71d
Anto jn <antojn19503@gmail.com>
2025-01-11 17:25:50 +0530

    first commit

---
+++ valid.js
@@ -0,0 +1,15 @@
+function validateForm() {
+   let v=document.forms["form"]
+   ["name"].value;
+   if(v== ''){
+    alert("jg");
+    return false;
+   }
+   ["email"].value
+   if(v==''){
+      alert("dsfa")
+      return false;
+   }
+   
+  
+}
